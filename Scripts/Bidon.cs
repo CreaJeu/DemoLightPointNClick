5c83c210982bf1b6d206ba300a3a9c6b40ed9d21
Ulrich Lejoyeux <ulrich.lejoyeux@steeple.fr>
2018-12-10 21:45:25 +0100

    add pointNclickable element

---
+++ Scripts/Bidon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bidon : PointNClickable {
+    public override void Interact()
+    {
+        Debug.Log("interaction with bidon");
+    }
+}
