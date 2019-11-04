3199ae150650cc2c7074cb12d30a43c496d5e8ba
ulejoyeu <ulrich.lejoyeux@gmail.com>
2019-11-04 23:38:29 +0100

    dish falls

---
+++ Scripts/Dish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Dish : PointNClickable
+{
+    public AnimationTransform animationTransform;
+    public AudioSource successSFX;
+
+    bool waitingToFall = false;
+
+    public override AudioSource Interact()
+    {
+        if (animationTransform.state == AnimationTransform.State.BEFORE)
+        {
+            animationTransform.state = AnimationTransform.State.MOVING;
+            return successSFX;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
